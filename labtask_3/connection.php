25bb593a54f95783e3f450e83e42723e4bd4611d
Tahsinul Hoque siddiki <tahsinulhoque10@gmail.com>
2025-06-23 00:34:17 +0600

    upload connection file

---
+++ labtask_3/connection.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Database connection parameters
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'myFirst';
+
+    $conn = new mysqli($host, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    ?>
+</body>
+</html>
