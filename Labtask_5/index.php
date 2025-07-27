37bf7a6a739398adc582363545dc21cac078d876
Tahsinul Hoque siddiki <tahsinulhoque10@gmail.com>
2025-07-27 17:22:09 +0600

    Index file

---
+++ Labtask_5/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Include the controller file
+include_once("controller/Controller.php");
+
+// Create a new instance of the Controller
+$controller = new Controller();
+
+// Call the invoke method to start the application logic
+$controller->invoke();
+
+?>
